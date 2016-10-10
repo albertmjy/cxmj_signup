5a673aa765536a80623e3955a27ed40847fb416b
U-CORP\junyma <junyma@L-SHC-15008571.corp.ebay.com>
2016-10-10 21:20:37 +0800

    add event.php

---
+++ event.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<title></title>
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/main.css" />
+	</head>
+
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">
+		      	<!--<img class="logo" src="img/getheadimg.jpeg" alt="logo" />-->
+		      	
+		      </a>
+		    </div>
+		  </div>
+		</nav>
+		<div class="container-fluid">
+		<div class="panel">
+			
+			<div class="panel-heading">
+				<img class="logo" src="img/1244782443.jpg" alt="logo" />
+			</div>
+			<div class="panel-body">
+				<img class="head-img" src="http://wx.qlogo.cn/mmopen/9M0PhLTmTIch24EL86awR6CUoGEpXaaSDFjNruCsQRNULdkJqWkQxIibUchz0h5vTI9jnhWfpStCAuia47xN7ZU2GqtP7FEegN/0" />
+				
+				<div> let's get this?</div>
+
+			</div>
+		
+		</div>
+		</div>
+		
+		<footer>
+			<nav class="navbar">
+				<hr />
+			  <p>&copy; 2016 Company, Inc.</p>
+		</nav>
+		</footer>
+		
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+	</body>
+
+</html>
